ce5cc72bd5d29a814413f64ddda724c9fc7a82b4
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2019-07-10 21:36:54 +0200

    Create statements.js

---
+++ statements.js
@@ -0,0 +1,12 @@
+var lengh = 2;
+var x = 0;
+x = Math.floor(Math.random() * lengh);
+
+
+var statments = [
+"Life is.. wait WHAAAAT???",
+"Existence implies influence, influence implies existence.",
+"Procrastination is my word."
+];
+
+document.getElementById("top").innerHTML = statements[x];
