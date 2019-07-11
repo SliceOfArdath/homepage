27b6c43695c9debca66282ae5472533f27ebd786
OscarHavril <OscarHavril@users.noreply.github.com>
2019-07-11 09:48:06 +0200

    Update statements.js

--- statements.js
+++ statements.js
@@ -1,10 +1,16 @@
-var lengh = 3;
+var lengh = 8;
 var x = Math.floor(Math.random() * lengh);
 
 var statments = [
 "Life is.. wait WHAAAAT???",
 "Existence implies influence, influence implies existence.",
-"Procrastination is my word."
+"Procrastination is my word.",
+"Any computer is a laptop if you're brave enough!",
+"Doesn't avoid double negatives!",
+"pls rt",
+"sqrt(-1) love you!",
+"Orthographicaly correct!",
+"This message will never appear on the splash screen, isn't that weird?"
 ];
 
 document.getElementById("top").innerHTML = statments[x];
