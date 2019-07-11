1448a71d27d89e007aed0c7da2ff1ab099bf0050
OscarHavril <OscarHavril@users.noreply.github.com>
2019-07-11 16:07:40 +0200

    another reference

--- statements.js
+++ statements.js
@@ -1,5 +1,8 @@
-var lengh = 11;
-var x = Math.floor(Math.random() * lengh);
+var l = 11;
+if (window.location.search == '?enhancedmode=true') {
+l++;
+    }
+var x = Math.floor(Math.random() * l);
 
 var statments = [
 "Life is.. wait WHAAAAT???",
@@ -12,7 +15,7 @@ var statments = [
 "Orthographicaly correct!",
 "PHP free!",
 "Being annoying is the best thing you could ever imagine",
-"There\'s really no hard limit to how long these achievement names can be and to be quite honest I\'m rather curious to see how far we can go. Adolphus W. Green (1844–1917) started as the Principal of the Groton School in 1864. By 1865, he became second assistant librarian at the New York Mercantile Library; from 1867 to 1869, he was promoted to full librarian. From 1869 to 1873, he worked for Evarts, Southmayd & Choate, a law firm co-founded by William M. Evarts, Charles Ferdinand Southmayd and Joseph Hodges Choate. He was admitted to the New York State Bar Association in 1873. Anyway, how\'s your day been?",
+"There\'s really no hard limit to how long these statements can be and to be quite honest I\'m rather curious to see how far we can go. Adolphus W. Green (1844–1917) started as the Principal of the Groton School in 1864. By 1865, he became second assistant librarian at the New York Mercantile Library; from 1867 to 1869, he was promoted to full librarian. From 1869 to 1873, he worked for Evarts, Southmayd & Choate, a law firm co-founded by William M. Evarts, Charles Ferdinand Southmayd and Joseph Hodges Choate. He was admitted to the New York State Bar Association in 1873. Anyway, how\'s your day been?",
 "Pro tip: the password is someone's name",
 "This message will never appear on the top bar, isn't that weird?"
 ];
