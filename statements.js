7ebeebf46b32914d7302a65fde17a9a26993d5e5
OscarHavril <OscarHavril@users.noreply.github.com>
2019-07-12 14:33:59 +0200

    tiny updates

--- statements.js
+++ statements.js
@@ -1,4 +1,4 @@
-var l = 14;
+var l = 15;
 if (window.location.search == '?enhancedmode=true') {
 l++;
 l++;
@@ -18,14 +18,10 @@ var statments = [
 "Being annoying is the best thing you could ever imagine",
 "There\'s really no hard limit to how long these statements can be and to be quite honest I\'m rather curious to see how far we can go. Adolphus W. Green (1844–1917) started as the Principal of the Groton School in 1864. By 1865, he became second assistant librarian at the New York Mercantile Library; from 1867 to 1869, he was promoted to full librarian. From 1869 to 1873, he worked for Evarts, Southmayd & Choate, a law firm co-founded by William M. Evarts, Charles Ferdinand Southmayd and Joseph Hodges Choate. He was admitted to the New York State Bar Association in 1873. Anyway, how\'s your day been?",
 "H̶̗̣̱̳̟̼̥̦̘̰̯̜̊̃́͊̀̇̈̿̓̆̾͘͜͠ͅǫ̷̨̼̪̰̮͍̮͚̮͙̮͉̗̪͖̝̞͖̭͇̬̩̫͖̩̼̟̫̱̒̓̈́͛̒́̒̒̿̀͊̊̓̒͛̂̅͆͑̿̑̃͘͜͝ͅw̶̢̛̛͎̘̣̼͖̦̖̜̩̩̤̬͎̰̗͇͍̰̗̹͈͚͉͖͈͉̣̐͌̿̈̽̿͗̑̽̃̇̂̓͗̀͌̽̀͗̀̒̑͛̿̚͜͠ͅͅ ̵̡̢͎̦̮̮̠̬̥̼̯̥͖̪̯̦̦͈̣̐̉̐̍̐͂͌͗̈̒̄͒́̍ͅḑ̴̛̛͓̖̠͉̅̓͒̀̋̈́͛͛̐̀̏̑̚͠͠͠o̶̢̨̨̡̦̲̟͍̩̜̲̳͓͚̙͇̱͔̱̼̣̱͎̪̫̲̬̰̿̉̉̎͌̈́̌̈́̊̈́͜͜͝ͅȩ̴̛̟̥͇̜̥̲̞̟̺̜͕̜̫͕̣̫͕̰̪̖͗̔͆̈́̓͒̈̈́͂̚̕̚ṣ̸̢̨͇̠̥̘̝͙̻̗͉̺̗̳̹͈̬̺̼͉̦͍͎̮̮̫͇̽͋̿̀͊̽̑͐́̇́̉̅͋̉͂͋̊̽̎̎̋̏̈͑͜͝ͅ ̵̦̣̏̽̄̈͒́͆̊̌́̓͘͝Z̸̨̛̲̳̫̞͖̬̣̳̤̩̖͛́̆̎̏̀͐̆̈́̓́̔̋͂͒̍̍͗͌̓̀͘͝͝ḁ̵̗͖̟͇̖̩̹̟̹̮͉̜̤̜̫̞͉̘̻̤͕̱̰̳̆̓̑͛̒̔̒̌̿̏̓̀̿̈̏́͆͜ͅl̶̢̡̨̢̨̛̛̼̼̙͇̪̝̮̠̩̤̮̭̤̰̗̠͎̣̮̞̻͚̻̘̼̳̔͆͒̔̑͒̑̀͋̓̀͂̄͊̋́̓͝g̴̨̱̦͚͔̫̥̙̬̺͎͐̐͋̇̈́̽͗͊̄̓͗̌̏̈́̅̈̽͗̀̋̅͊̿̅͐͘͘͘͜͝ò̸̻̰̈́̌ ̴̛͙̱̳͖̹̺̊̑̊̋̏͗́͌̚͘t̴̡̨̧̨̬̜̱̱̹̤̥͍̪̖̪̰̜̹̪͔̻͖̭͆̔͒̐̊̆͗̆̅́̓̕͝ͅe̸̛̙̳̤̠̳͐̓̓̈́͗͗̈́͗̍̄͐̅͋͂̈́̀͒̏̊̾̅͘͘͝͠͝ͅx̴̧̺̖̙͇̖̼̦̯͉͕̝̗͍̔͛̉̒͋̒́͗̂͆͑̀̑̌͊͒̓̽̍͌̈́̈̚̕͠͝ͅţ̵̡̡̡͈̞̦͈̗̥͇͇͙͚̅͛̉̒̀͗̄̇̓̈́̎̅ ̵̡̢̛͕̗̝̻͙̯͖̯̝͈̰͂̌̐̿͌̔ẅ̷̛̺́͛̈͌̅̎̍̊̓͋̒̎̑̔͌̓̒͂͗̏̊̕̕͠ơ̴͈̥̣̈́̏̊̽͗̒̇͑̎̋͂͐̊͝r̴̢̧͇̣͚̭̼̲̥̫̣͇̜̪̭̻͙̬̫̯̝̃̇͐̍͐́̓ͅk̵̨̤̭̩͙̦̻͈̗̹̣͉̯̦͕̺̹̙͓̲̲̮̃͗̓̀͂̓̈́̄̑͋̿̅̉̔̄͝͝?̷̝̞̟̻̘̻͚̭̟̯̯̲̼̭̲͙̫̳͕̰͖̥̏̃͊̍̏͑̇̑̎̄̂̉́̎͌̈́̽̋͗̒̏̎͘͘͜ͅ ̶̧̢̢̡̛̺̟̭͓̺̬͉͕͍̻̘͔̙̱̘͕͔̤̥̳̬̭̯̰̯̏̋̓͐̇̈́̒̊̓̅̾̈̃̉͘̚͜",
-
-
-
-
-
-
+"150% hyperbole!",
 '<span style="color:#0000AA;">C</span><span style="color:#00AA00;">o</span><span style="color:#00AAAA;">l</span><span style="color:#AA0000;">o</span><span style="color:#AA00AA;">r</span><span style="color:#FFAA00;">m</span><span style="color:#AAAAAA;">a</span><span style="color:#555555;">t</span><span style="color:#5555FF;">i</span><span style="color:#55FF55;">c</span>',
 "It's dangerous to go alone! Take <a href='ref.html'><b>this.</b></a>",
+
 "Pro tip: the password is someone's name",
 "Did I mention the name was followed by a number between 0 and 9? Oh yeah.",
 "This message will never appear on the top bar, isn't that weird?"
