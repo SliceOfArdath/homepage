0d16c084fde306a94e021d2cb2bd5f125734a2bc
OscarHavril <OscarHavril@users.noreply.github.com>
2019-07-12 10:46:45 +0200

    big upgrades!
    -switched to SHA-256!
    -added enter detection on password field!
    -added new statements!

--- statements.js
+++ statements.js
@@ -1,5 +1,6 @@
-var l = 12;
+var l = 13;
 if (window.location.search == '?enhancedmode=true') {
+l++;
 l++;
     }
 var x = Math.floor(Math.random() * l);
@@ -17,7 +18,15 @@ var statments = [
 "Being annoying is the best thing you could ever imagine",
 "There\'s really no hard limit to how long these statements can be and to be quite honest I\'m rather curious to see how far we can go. Adolphus W. Green (1844–1917) started as the Principal of the Groton School in 1864. By 1865, he became second assistant librarian at the New York Mercantile Library; from 1867 to 1869, he was promoted to full librarian. From 1869 to 1873, he worked for Evarts, Southmayd & Choate, a law firm co-founded by William M. Evarts, Charles Ferdinand Southmayd and Joseph Hodges Choate. He was admitted to the New York State Bar Association in 1873. Anyway, how\'s your day been?",
 "H̶̗̣̱̳̟̼̥̦̘̰̯̜̊̃́͊̀̇̈̿̓̆̾͘͜͠ͅǫ̷̨̼̪̰̮͍̮͚̮͙̮͉̗̪͖̝̞͖̭͇̬̩̫͖̩̼̟̫̱̒̓̈́͛̒́̒̒̿̀͊̊̓̒͛̂̅͆͑̿̑̃͘͜͝ͅw̶̢̛̛͎̘̣̼͖̦̖̜̩̩̤̬͎̰̗͇͍̰̗̹͈͚͉͖͈͉̣̐͌̿̈̽̿͗̑̽̃̇̂̓͗̀͌̽̀͗̀̒̑͛̿̚͜͠ͅͅ ̵̡̢͎̦̮̮̠̬̥̼̯̥͖̪̯̦̦͈̣̐̉̐̍̐͂͌͗̈̒̄͒́̍ͅḑ̴̛̛͓̖̠͉̅̓͒̀̋̈́͛͛̐̀̏̑̚͠͠͠o̶̢̨̨̡̦̲̟͍̩̜̲̳͓͚̙͇̱͔̱̼̣̱͎̪̫̲̬̰̿̉̉̎͌̈́̌̈́̊̈́͜͜͝ͅȩ̴̛̟̥͇̜̥̲̞̟̺̜͕̜̫͕̣̫͕̰̪̖͗̔͆̈́̓͒̈̈́͂̚̕̚ṣ̸̢̨͇̠̥̘̝͙̻̗͉̺̗̳̹͈̬̺̼͉̦͍͎̮̮̫͇̽͋̿̀͊̽̑͐́̇́̉̅͋̉͂͋̊̽̎̎̋̏̈͑͜͝ͅ ̵̦̣̏̽̄̈͒́͆̊̌́̓͘͝Z̸̨̛̲̳̫̞͖̬̣̳̤̩̖͛́̆̎̏̀͐̆̈́̓́̔̋͂͒̍̍͗͌̓̀͘͝͝ḁ̵̗͖̟͇̖̩̹̟̹̮͉̜̤̜̫̞͉̘̻̤͕̱̰̳̆̓̑͛̒̔̒̌̿̏̓̀̿̈̏́͆͜ͅl̶̢̡̨̢̨̛̛̼̼̙͇̪̝̮̠̩̤̮̭̤̰̗̠͎̣̮̞̻͚̻̘̼̳̔͆͒̔̑͒̑̀͋̓̀͂̄͊̋́̓͝g̴̨̱̦͚͔̫̥̙̬̺͎͐̐͋̇̈́̽͗͊̄̓͗̌̏̈́̅̈̽͗̀̋̅͊̿̅͐͘͘͘͜͝ò̸̻̰̈́̌ ̴̛͙̱̳͖̹̺̊̑̊̋̏͗́͌̚͘t̴̡̨̧̨̬̜̱̱̹̤̥͍̪̖̪̰̜̹̪͔̻͖̭͆̔͒̐̊̆͗̆̅́̓̕͝ͅe̸̛̙̳̤̠̳͐̓̓̈́͗͗̈́͗̍̄͐̅͋͂̈́̀͒̏̊̾̅͘͘͝͠͝ͅx̴̧̺̖̙͇̖̼̦̯͉͕̝̗͍̔͛̉̒͋̒́͗̂͆͑̀̑̌͊͒̓̽̍͌̈́̈̚̕͠͝ͅţ̵̡̡̡͈̞̦͈̗̥͇͇͙͚̅͛̉̒̀͗̄̇̓̈́̎̅ ̵̡̢̛͕̗̝̻͙̯͖̯̝͈̰͂̌̐̿͌̔ẅ̷̛̺́͛̈͌̅̎̍̊̓͋̒̎̑̔͌̓̒͂͗̏̊̕̕͠ơ̴͈̥̣̈́̏̊̽͗̒̇͑̎̋͂͐̊͝r̴̢̧͇̣͚̭̼̲̥̫̣͇̜̪̭̻͙̬̫̯̝̃̇͐̍͐́̓ͅk̵̨̤̭̩͙̦̻͈̗̹̣͉̯̦͕̺̹̙͓̲̲̮̃͗̓̀͂̓̈́̄̑͋̿̅̉̔̄͝͝?̷̝̞̟̻̘̻͚̭̟̯̯̲̼̭̲͙̫̳͕̰͖̥̏̃͊̍̏͑̇̑̎̄̂̉́̎͌̈́̽̋͗̒̏̎͘͘͜ͅ ̶̧̢̢̡̛̺̟̭͓̺̬͉͕͍̻̘͔̙̱̘͕͔̤̥̳̬̭̯̰̯̏̋̓͐̇̈́̒̊̓̅̾̈̃̉͘̚͜",
+
+
+
+
+
+
+'<span style="color:#0000AA;">C</span><span style="color:#00AA00;">o</span><span style="color:#00AAAA;">l</span><span style="color:#AA0000;">o</span><span style="color:#AA00AA;">r</span><span style="color:#FFAA00;">m</span><span style="color:#AAAAAA;">a</span><span style="color:#555555;">t</span><span style="color:#5555FF;">i</span><span style="color:#55FF55;">c</span>',
 "Pro tip: the password is someone's name",
+"Did i mention the name was followed by a number between 0 and 9? Oh yeah.",
 "This message will never appear on the top bar, isn't that weird?"
 ];
 
