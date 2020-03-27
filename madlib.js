2078cbdf317fbb6ed27293b7a2d9a0b675ed208d
OscarHavril <OscarHavril@users.noreply.github.com>
2020-03-27 12:33:41 +0100

    Update madlib.js

--- madlib.js
+++ madlib.js
@@ -127,10 +127,14 @@ var sound = {
     play: function (buffer, vol = 1, loop = false) {
         window.AudioContext = window.AudioContext || window.webkitAudioContext; context = new AudioContext();
         var source = context.createBufferSource();
-        eval("sound.sources." + buffer + " = sound.createSource(sound.bank." + buffer + ");");
-        eval("sound.sources." + buffer + ".source.loop = loop;");
-        eval("sound.sources." + buffer + ".gainNode.gain.value = vol * vol;");
-        eval("sound.sources." + buffer + ".source.start(0);");
+        sound.sources[buffer] = sound.createSource(sound.bank[buffer]);
+        sound.sources[buffer].source.loop = loop;
+        sound.sources[buffer].gainNode.gain.value = vol * vol;
+        sound.sources[buffer].source.start(0);
+        //eval("sound.sources." + buffer + " = sound.createSource(sound.bank." + buffer + ");");
+        //eval("sound.sources." + buffer + ".source.loop = loop;");
+        //eval("sound.sources." + buffer + ".gainNode.gain.value = vol * vol;");
+        //eval("sound.sources." + buffer + ".source.start(0);");
         //setTimeout(eval("sound.sources." + buffer + " = sound.createSource(sound.bank." + buffer + ");"), eval("sound.bank." + buffer + ".duration") * 1000)
     },
     pause: function (buffer) {
